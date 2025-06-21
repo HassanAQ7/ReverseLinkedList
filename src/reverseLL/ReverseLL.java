4a136a6f35a9a3123a31f36077e760775ff06435
HassanAQ7 <hassan.allyqureshi@gmail.com>
2025-06-21 13:34:13 -0700

    implement reverseLL method in ReverseLL class, test case passing

---
+++ src/reverseLL/ReverseLL.java
@@ -0,0 +1,25 @@
+package reverseLL;
+
+public class ReverseLL {
+
+    public class LLNode {
+        public int val;
+        public LLNode next;
+
+        public LLNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public LLNode reverseLL(LLNode head) {
+        LLNode prev = null;
+        while (head != null) {
+            LLNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+
+        return prev;
+    }
+}
